34172b3b33c0c50f1b3770aecfa238e8c2c16322
Thomas Moraes <tho.vini7@gmail.com>
2026-04-24 18:01:58 -0300

    fix: Room Entity tests

---
+++ src/domain/entities/Room.ts
@@ -0,0 +1,34 @@
+import Uuid from "../value-objects/Uuid";
+
+export default class Room {
+    constructor(
+        readonly uuid: Uuid,
+        readonly number: number,
+        readonly capacity: number,
+        readonly pricePerNight: number,
+        private status: "available" | "occupied"
+    ) {}
+    
+    static create(number: number, capacity: number, pricePerNight: number): Room {
+        const uuid = Uuid.create();
+        return new Room(uuid, number, capacity, pricePerNight, "available");
+    }
+
+    getUuid(): string {
+        return this.uuid.getValue();
+    }
+
+    getStatus(): string {
+        return this.status;
+    }
+
+    occupy(): void {
+        if (this.status === "occupied") throw new Error("Room is already occupied");
+        this.status = "occupied";
+    }
+
+    available(): void {
+        if (this.status === "available") throw new Error("Room is already available");
+        this.status = "available";
+    }
+}
---
+++ test/unit/Room.test.ts
@@ -0,0 +1,35 @@
+import Room from "../../src/domain/entities/Room";
+
+it("should create a room", () => {
+    const room = Room.create(101, 1, 55);
+    expect(room).toBeInstanceOf(Room);
+    expect(room.getUuid()).toBeDefined();
+    expect(room.number).toBe(101);
+    expect(room.capacity).toBe(1);
+    expect(room.pricePerNight).toBe(55);
+    expect(room.getStatus()).toBe("available");
+});
+
+it("should occupy a room", () => {
+    const room = Room.create(101, 1, 55);
+    room.occupy();
+    expect(room.getStatus()).toBe("occupied");
+});
+
+it("should not occupy an already occupied room", () => {
+    const room = Room.create(101, 1, 55);
+    room.occupy();
+    expect(() => room.occupy()).toThrow("Room is already occupied");
+});
+
+it("should make a room available", () => {
+    const room = Room.create(101, 1, 55);
+    room.occupy();
+    room.available();
+    expect(room.getStatus()).toBe("available");
+});
+
+it("should not make an already available room available", () => {
+    const room = Room.create(101, 1, 55);
+    expect(() => room.available()).toThrow("Room is already available");
+});
